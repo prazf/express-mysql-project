4e88bfa29a0c8dc967059e74a0dc2926631796d0
Pradnya Shirodkar <prads.999@gmail.com>
2019-12-09 10:53:41 +0530

    duplicate line removed

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ const app = express();
 const router = require('./routes');
 app.use(express.json());
 app.use(router);
-app.use(express.json());
-
 const port = 5000;
 
 // configure middleware
 app.set('port', process.env.port || port); // set express to use this port
-
-
-
 // set the app to listen on the port
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
